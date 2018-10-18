ae940936b773de2575e74a8b3427fcb11c876c80
jgirardet <ijkl@netc.fr>
2018-10-18 21:51:59 +0200

    unleash command

--- poem/__init__.py
+++ poem/__init__.py
@@ -1,6 +1,6 @@
 from .consts import PACKAGE_NAME
 from .utils import get_settings
-# from .commands import PoemSetPythonInterpreterCommand
+from .commands import PoemSetPythonInterpreterCommand
 
-__all__ = ['PACKAGE_NAME', "get_settings"]
-# __all__ = ['PACKAGE_NAME', "get_settings", "PoemSetPythonInterpreterCommand"]
+# __all__ = ['PACKAGE_NAME', "get_settings"]
+__all__ = ['PACKAGE_NAME', "get_settings", "PoemSetPythonInterpreterCommand"]

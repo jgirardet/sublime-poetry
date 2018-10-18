efab75188f7523f5dada9fef5dc408256d519398
jgirardet <ijkl@netc.fr>
2018-10-18 21:55:21 +0200

    log

--- main.py
+++ main.py
@@ -17,26 +17,26 @@ if not os.environ.get("CI", None):
     LOG.propagate = False
 
 
-# def plugin_loaded():
-#     # load config
-#     current_view = sublime.active_window().active_view()
-#     config = get_settings(current_view)
+def plugin_loaded():
+    # load config
+    current_view = sublime.active_window().active_view()
+    config = get_settings(current_view)
 
-#     # Setup  logging
-#     if not LOG.handlers:
-#         debug_formatter = logging.Formatter(
-#             "[{}:%(filename)s](%(levelname)s) %(message)s".format(PACKAGE_NAME)
-#         )
-#         dh = logging.StreamHandler()
-#         dh.setLevel(logging.DEBUG)
-#         dh.setFormatter(debug_formatter)
-#         LOG.addHandler(dh)
+    # Setup  logging
+    if not LOG.handlers:
+        debug_formatter = logging.Formatter(
+            "[{}:%(filename)s](%(levelname)s) %(message)s".format(PACKAGE_NAME)
+        )
+        dh = logging.StreamHandler()
+        dh.setLevel(logging.DEBUG)
+        dh.setFormatter(debug_formatter)
+        LOG.addHandler(dh)
 
-#     try:
-#         LOG.setLevel(config.get("poem_log", "").upper())
-#     except ValueError as err:
-#         LOG.error(err)
-#         LOG.setLevel("ERROR")
-#         LOG.error("fallback to loglevel ERROR")
+    try:
+        LOG.setLevel(config.get("poem_log", "").upper())
+    except ValueError as err:
+        LOG.error(err)
+        LOG.setLevel("ERROR")
+        LOG.error("fallback to loglevel ERROR")
 
-#     LOG.info("Loglevel set to %s", config["poem_log"].upper())
+    LOG.info("Loglevel set to %s", config["poem_log"].upper())
